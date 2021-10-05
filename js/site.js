440510dbfb52ba289951cd82a7797d8eefd92f1d
jordanthopps <91483280+jordanthopps@users.noreply.github.com>
2021-10-05 15:47:51 -0400

    Commenting out Site.JS

--- js/site.js
+++ js/site.js
@@ -1,16 +1,52 @@
-function displayMessage() {
-    let msg = ""
+//There are three functions that together amass the Rewind application:
 
-    msg = document.getElementById("message").value;
 
-    alert(msg);
+1.
+/*Controller Function
+   In app.html, the input id has been set to: userString. The userString is what the user types into the text field.
+   In this function, that userString value is assigned to the userString variable (line 11)*/
+function getString() {
+
+    //(a) Assigns user's input to the variable userString.
+    let userString = document.getElementById("userString").value;
+
+    //(b) userString becomes an argument for a function, reverseString. Thank value is assigned to the variable revString
+    let revString = reverseString(userString);
+
+    //(c) revString becomes an argument for a function, reverseString.
+    displayString(revString);
+}
+/*Note: what the getString function above does is takes the user's input (a) > passes that input to the for loop function (b) >
+        then passes that value to the display function to appear on the user's screen (c)*/
+
+//Definition of the function that reverses the user's input:
+function reverseString(userString) {
+    let startValue = userString.length - 1; //A string is an array.
+    let endValue = 0; //The first position of a string in an array is 0
+
+    //Let's assume that the user typed in 'Frank'
+    //Frank is a string AND an array of characters
+    //[F, r, a, n, k]
+    //So the start value will need to be userString.length - 1 becuase the length of a string will always be 1 more than the indexed array length
+    let revString = "";
+    //In a decremented for loop, the startValue must be higher than the endValue
+    for (let i = startValue; i >= endValue; i--) {
+        revString += userString[i]; //userString[i] is added to revString with augmented addition
+
+    };
+
+    return revString
+    //Loop 1 - "k"
+    //Loop 2 - "kn"
+    //Loop 3 - "kna"
+    //Loop 4 - "knar"
+    //Loop 5 - "knarF"
+
 }
 
-function displayMessage2() {
-    let msg = "";
+//Definition of the function that displays the target result:
+function displayString(revString) {
+    let output = document.getElementById("results"); //line 79 in app.html, the paragraphed return was assigned id="results"
 
-    msg = document.getElementById("message").value;
-
-    Swal.fire(msg);
-
+    output.innerHTML = revString;
 }
